cc9940c1df7682e934dcdb36d4341e3f609faebf
Benat Castro <benatcastro@gmail.com>
2024-04-25 23:23:09 +0200

    spring files

---
+++ api/src/main/java/com/disrupt/daedalus/api/ApiApplication.java
@@ -0,0 +1,13 @@
+package com.disrupt.daedalus.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
+
+}
