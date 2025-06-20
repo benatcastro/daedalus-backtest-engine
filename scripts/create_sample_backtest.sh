db5a682836f1d1b5c50529c8c5cdc8db6809e6f2
benatcastro <benatcastro@gmail.com>
2025-06-20 23:13:02 +0200

    feat: added script to reset the backtest db

--- scripts/create_sample_backtest.sh
+++ scripts/create_sample_backtest.sh
@@ -4,13 +4,13 @@ curl -X POST http://localhost:8000/api/v1/backtest/ \
                                                                 -F "strategy_id=1" \
                                                                 -F "name=Lean Backtest Upload Test" \
                                                                 -F "description=Testing upload with actual sample files" \
-                                                                -F "files=@/home/bena/Projects/daedalus/backtest-backend/sample/backtest/1217966458-log.txt" \
-                                                                -F "files=@/home/bena/Projects/daedalus/backtest-backend/sample/backtest/1217966458-order-events.json" \
-                                                                -F "files=@/home/bena/Projects/daedalus/backtest-backend/sample/backtest/1217966458-summary.json" \
-                                                                -F "files=@/home/bena/Projects/daedalus/backtest-backend/sample/backtest/1217966458.json" \
-                                                                -F "files=@/home/bena/Projects/daedalus/backtest-backend/sample/backtest/data-monitor-report-20250407194012683.json" \
-                                                                -F "files=@/home/bena/Projects/daedalus/backtest-backend/sample/backtest/succeeded-data-requests-20250407193956377.txt" \
-                                                                -F "files=@/home/bena/Projects/daedalus/backtest-backend/sample/backtest/failed-data-requests-20250407193956377.txt" \
-                                                                -F "files=@/home/bena/Projects/daedalus/backtest-backend/sample/backtest/log.txt" \
-                                                                -F "files=@/home/bena/Projects/daedalus/backtest-backend/sample/backtest/config" \
+                                                                -F "files=@/home/bena/Projects/daedalus/backtest-backend/sample/1217966458-log.txt" \
+                                                                -F "files=@/home/bena/Projects/daedalus/backtest-backend/sample/1217966458-order-events.json" \
+                                                                -F "files=@/home/bena/Projects/daedalus/backtest-backend/sample/1217966458-summary.json" \
+                                                                -F "files=@/home/bena/Projects/daedalus/backtest-backend/sample/1217966458.json" \
+                                                                -F "files=@/home/bena/Projects/daedalus/backtest-backend/sample/data-monitor-report-20250407194012683.json" \
+                                                                -F "files=@/home/bena/Projects/daedalus/backtest-backend/sample/succeeded-data-requests-20250407193956377.txt" \
+                                                                -F "files=@/home/bena/Projects/daedalus/backtest-backend/sample/failed-data-requests-20250407193956377.txt" \
+                                                                -F "files=@/home/bena/Projects/daedalus/backtest-backend/sample/log.txt" \
+                                                                -F "files=@/home/bena/Projects/daedalus/backtest-backend/sample/config" \
                                                                 -H "Accept: application/json"
